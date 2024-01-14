05691ba669d30ec652e51d5b550479028757ded5
leracereusenco <144594148+leracereusenco@users.noreply.github.com>
2024-01-14 12:13:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// var n = 12;  //variabila se declara prin = , se numeste variabila
+// let m = 12;  //forma noua a lui var, se numeste variabila
+// const b = 12; //are o valoare constanta, se numeste constanta
+// console.log(n);
+
+// n=23
+// console.log(n);
+
+// n=67
+// console.log(n);
+
+
+//ctrl + / = comentariul la tot textul ales
+
+/*
+text
+comentat
+mai multe randuri la un loc
+*/
+
+
+/*
+let string = "text";
+let num = 3; //нет разделения между int и float
+let n = null; //пустой объект
+let m; //variabila fara valoare
+let boolean = true/false;
+
+console.log(typeof m); //verificarea tipului variabilei
+*/
+
+
+
+console.log(5 + "5" + 3); //se alipeste ca text
+
+console.log("26" - 16); //26 se face numar
+
+console.log("26" * "2"); //ambele se transforma in numere
+
+
+/*
++
+-
+*
+/
+** - pow
+x = x + y  == x += y
+x = x - y  == x -= y
+*/
